8946dec606fe176d6e0871c69b921a7751d33fc6
Manoranjan <manoranjanmishra@cime.ac.in>
2024-05-07 22:28:43 +0530

    const

--- fe/src/constants.js
+++ fe/src/constants.js
@@ -1,14 +1,14 @@
 const uri = {
   english: {
-    teen: ["SfPGQN5wKFs", "TuUVVKVdZm4", "Dp6lbdoprZ0", "yApIDzAO6YU"],
-    middleAged: ["kw4tT7SCmaY", "8c6GRDxa6HQ", "G6SutyLlzws", "lN1m7zLBbSU"],
-    oldAged: ["kw4tT7SCmaY", "vKDsAB1ccn0"],
+    teen: ["oxBuWZtjl0U", "y-qVC75Z5a8", "G8ztZoFQXco", "ILRo39a3gyg", "-NOQCNWckJs"],
+    middleAged: ["oxBuWZtjl0U", "TJsry-wIDa4", "AHgBY4UwS6U", "X3cYGtlp5so", "ZVFBMXi1FTE", "7T_jkMhG2PU", "NQsY8IA20nw", "hgIAYgR-btA", "XSZeZ3VtV6Y", "0RgcN7qJddo"],
+    oldAged: ["oxBuWZtjl0U", "fMxOJ8Sf_ck", "o9xrgZCCSsw","ILRo39a3gyg", "-NOQCNWckJs"],
   },
   hindi: {
-    teen: ["SfPGQN5wKFs", "TuUVVKVdZm4", "Dp6lbdoprZ0", "yApIDzAO6YU"],
-    middleAged: ["kw4tT7SCmaY", "8c6GRDxa6HQ", "G6SutyLlzws", "lN1m7zLBbSU"],
-    oldAged: ["kw4tT7SCmaY", "vKDsAB1ccn0", "SfPGQN5wKFs"],
-  },
+    oldAged: ["9eoZBhUNJbI", "QkUuhceGGC0", "4D9zGr91UwY", "NWfYC_HRPgM", "OLsfFoZRsIw"],
+    middleAged: ["9eoZBhUNJbI", "vTz63cgADiw", "4W2eXgZiWbE", "5vgVNFoL86U", "f1fToNHFxGk", "Cg6MmVx8N0c", "g4WwIL_SUww", "YgHj29gFRGc", "pmwvBeY2dgU", "n70lPPuoKGA"],
+    teen: ["9eoZBhUNJbI", "UL2UnmbX8t4", "lyJ2mynaqfc", "NWfYC_HRPgM", "OLsfFoZRsIw"],
+  }, 
 };
 
 export default uri;
